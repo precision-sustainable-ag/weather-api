db3e9fb2edac85729e2d48fcfc71f6642865ff49
Rick Hitchcock <rickhitchcock@outlook.com>
2023-10-12 13:54:52 -0400

    routePlantsTable

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ app.all('/plants', db.routePlants);
 app.all('/plants2', db.routePlants2);
 app.all('/plantsrecords', db.initializeVariables, db.routePlantsRecords);
 app.all('/plantsstructure', db.initializeVariables, db.routePlantsStructure);
+app.all('/plantstable', db.initializeVariables, db.routePlantsTable);
 
 app.all('/yearly', db.initializeVariables, db.routeYearly);
 
