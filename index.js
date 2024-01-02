0f667fa82b351156d7fd93275932e3517e70991a
Rick Hitchcock <rickhitchcock@outlook.com>
2024-01-02 13:30:56 -0500

    state symbols and cultivars

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const path = require('path'); // to get the current path
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => { // next is unused but required!
